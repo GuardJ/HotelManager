711d8844dcb636b41afb19e5abdb8f2576e84498
GuardJ <guardadojose92@yahoo.com>
2015-09-29 21:27:07 -0700

    Add Label Change on ButtonClick

--- HotelManager/Form1.cs
+++ HotelManager/Form1.cs
@@ -16,5 +16,10 @@ namespace HotelManager
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "OKAY!";
+        }
     }
 }
